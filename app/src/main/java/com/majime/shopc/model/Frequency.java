f345f83c4c77a0fd1e81925f3d3756d72a400ec2
farelreza <rezafarel320@gmail.com>
2020-12-08 12:08:29 +0700

    add class frequency, memory, product, ram, socket, storage and vga

---
+++ app/src/main/java/com/majime/shopc/model/Frequency.java
@@ -0,0 +1,8 @@
+package com.majime.shopc.model;
+
+public interface Frequency {
+    public final int FREQUENCY = 3;
+
+    public void setFrequency(int Frequency);
+    public int getFrequency();
+}
